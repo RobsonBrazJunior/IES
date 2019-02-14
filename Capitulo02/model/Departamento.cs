9ecda6afd3eb8c0e98abd6160cdcebc74a1935f2
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-02-14 16:01:39 -0300

    Implementado Campo Cursos na Classe Departamento e implementado os métodos para registro e retorno

--- Capitulo02/model/Departamento.cs
+++ Capitulo02/model/Departamento.cs
@@ -1,7 +1,25 @@
-﻿namespace Capitulo02.model
+﻿using System.Collections.Generic;
+
+namespace Capitulo02.model
 {
     public class Departamento
     {
         public string Nome { get; set; }
+        public IList<Curso> Cursos { get; } = new List<Curso>();
+
+        public void RegistrarCurso(Curso c)
+        {
+            Cursos.Add(c);
+        }
+
+        public int ObterQunatidadeDeCursos()
+        {
+            return Cursos.Count;
+        }
+
+        public Curso ObterCursosPorIndice(int indice)
+        {
+            return Cursos[indice];
+        }
     }
 }
