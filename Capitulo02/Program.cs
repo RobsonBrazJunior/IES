d80f8e00970caa2a805faff3f6beedbf2ce4e547
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-02-18 10:57:34 -0400

    Realizando teste de registro ducpliacdo de Disciplina

--- Capitulo02/Program.cs
+++ Capitulo02/Program.cs
@@ -102,6 +102,44 @@ namespace Capitulo02
             Console.WriteLine("=======================================");
             Console.WriteLine();
 
+            var cursoCC = new Curso()
+            {
+                Nome = "Ciência da Computação",
+                CargaHoraria = 3000
+            };
+
+            cursoCC.RegistrarDisciplina(new Disciplina()
+            {
+                Nome = "Algoritmos",
+                CargaHoraria = 80
+            });
+            cursoCC.RegistrarDisciplina(new Disciplina()
+            {
+                Nome = "Orientação a Objetos",
+                CargaHoraria = 60
+            });
+            cursoCC.RegistrarDisciplina(new Disciplina()
+            {
+                Nome = "Orientação a Objetos",
+                CargaHoraria = 80
+            });
+            cursoCC.RegistrarDisciplina(new Disciplina()
+            {
+                Nome = "Estrutura de Dados",
+                CargaHoraria = 80
+            });
+            cursoCC.RegistrarDisciplina(new Disciplina()
+            {
+                Nome = "Programação para Web",
+                CargaHoraria = 80
+            });
+
+            Console.WriteLine($"O curso {cursoCC.Nome} possui {cursoCC.Disciplinas.Count} disciplinas: ");
+            foreach (var d in cursoCC.Disciplinas)
+            {
+                Console.WriteLine($"==> {d.Nome} ({d.CargaHoraria})");
+            }
+
             Console.Write("Pressione qualquer tecla para continuar");
             Console.ReadKey();
         }
