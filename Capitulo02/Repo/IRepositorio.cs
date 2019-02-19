b37b968a4db1159f26ff5f72ff6f0cf1d4a6a74f
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-02-19 16:13:21 -0400

    Criada interface IRepositorio

---
+++ Capitulo02/Repo/IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Capitulo02.Repo
+{
+    public interface IRepositorio<T>
+    {
+        T ObterPorId(string id);
+        IEnumerable<T> ObterTodos();
+        void Gravar(T objeto);
+        void Remover(T objeto);
+    }
+}
