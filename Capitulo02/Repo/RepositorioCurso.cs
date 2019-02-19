93a3b40c9bf69912053e9704b7fd03c793944d98
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-02-19 16:18:53 -0400

    Criada classe RepositorioCurso

---
+++ Capitulo02/Repo/RepositorioCurso.cs
@@ -0,0 +1,32 @@
+﻿using Capitulo02.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Capitulo02.Repo
+{
+    public class RepositorioCurso : IRepositorio<Curso>
+    {
+        public HashSet<Curso> Cursos { get; } = new HashSet<Curso>();
+
+        public void Gravar(Curso curso)
+        {
+            Cursos.Add(curso);
+        }
+
+        public Curso ObterPorId(string nome)
+        {
+            return Cursos.Where(c => c.Nome.Equals(nome)).FirstOrDefault();
+        }
+
+        public IEnumerable<Curso> ObterTodos()
+        {
+            return Cursos;
+        }
+
+        public void Remover(Curso curso)
+        {
+            Cursos.Remove(curso);
+        }
+    }
+}
