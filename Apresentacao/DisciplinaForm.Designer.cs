4d850dea7a96d6d964a50e600754d2fbac3ec400
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-02-20 16:19:47 -0400

    Criado formulário de Apresentação

---
+++ Apresentacao/DisciplinaForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Apresentacao
+{
+    partial class DisciplinaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCargaHoraria = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.dgvDisciplinas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisciplinas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Carga-horária:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(110, 29);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(254, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // txtCargaHoraria
+            // 
+            this.txtCargaHoraria.Location = new System.Drawing.Point(110, 57);
+            this.txtCargaHoraria.Name = "txtCargaHoraria";
+            this.txtCargaHoraria.Size = new System.Drawing.Size(160, 20);
+            this.txtCargaHoraria.TabIndex = 3;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(289, 55);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 4;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvDisciplinas
+            // 
+            this.dgvDisciplinas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisciplinas.Location = new System.Drawing.Point(15, 84);
+            this.dgvDisciplinas.Name = "dgvDisciplinas";
+            this.dgvDisciplinas.Size = new System.Drawing.Size(349, 320);
+            this.dgvDisciplinas.TabIndex = 5;
+            // 
+            // DisciplinaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 416);
+            this.Controls.Add(this.dgvDisciplinas);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtCargaHoraria);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DisciplinaForm";
+            this.Text = "DisciplinaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisciplinas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCargaHoraria;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.DataGridView dgvDisciplinas;
+    }
+}
